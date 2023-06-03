e3c9467afe5617e81750d6279080d9b71481f72c
Lucas <lucascostamonteiro@yahoo.com>
2023-06-02 21:18:50 -0700

    googlemaps

--- Frontend/vite-project/src/components/GoogleMaps/index.tsx
+++ Frontend/vite-project/src/components/GoogleMaps/index.tsx
@@ -1,45 +1,59 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
-import { business } from "./exampleFile";
+// import { business } from "./mockData";
 import "./GoogleMaps.css";
 
-const latMarker = business[0].coordinates.latitude;
-const lngMarker = business[0].coordinates.longitude;
+// const latMarker: number = business[0].coordinates.latitude;
+// const lngMarker: number = business[0].coordinates.longitude;
 
 // console.log(latMarker, lngMarker);
 
 const containerStyle = {
-  width: "100%",
   height: "100vh",
+  width: "100%",
 };
 
-const center = {
-  lat: 37.09024,
-  lng: -95.712891,
-};
-
-const markerPosition = {
-  lat: latMarker,
-  lng: lngMarker,
-};
+// const markerPosition = {
+//   lat: latMarker,
+//   lng: lngMarker,
+// };
+const locations = [
+  {
+    lat: 37.772,
+    lng: -122.214,
+  },
+  {
+    lat: 37.672,
+    lng: -122.219,
+  },
+  {
+    lat: 37.832,
+    lng: -122.424,
+  },
+];
 
 const TripGoogleMaps: React.FC = () => {
+  const center = useMemo(() => ({ lat: 37.09024, lng: -95.712891 }), []);
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: "AIzaSyAGiZ0jn302vwLSuChWuuRziuxMWfvEmKs",
   });
+
 
   const mapProps = {
     center,
     zoom: 4.5,
   };
 
-  const onLoad = (marker) => {
+  const onLoad = (marker: any) => {
     console.log("marker: ", marker);
   };
 
   return isLoaded ? (
     <GoogleMap mapContainerStyle={containerStyle} {...mapProps}>
-      <Marker position={markerPosition} onLoad={onLoad} />
+      {locations?.length &&
+        locations?.map((markerPos) => (
+          <Marker onLoad={onLoad} position={markerPos} />
+        ))}
     </GoogleMap>
   ) : (
     <div>Loading...</div>
