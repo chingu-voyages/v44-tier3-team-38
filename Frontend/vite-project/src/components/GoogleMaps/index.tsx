dd18c07d63f72a2896fca63c1e56cb7edb2e2c7e
Trevor Mathiak <tmathiak@gmail.com>
2023-06-05 23:31:25 -0400

    Fixed google maps marker functionality

--- Frontend/vite-project/src/components/GoogleMaps/index.tsx
+++ Frontend/vite-project/src/components/GoogleMaps/index.tsx
@@ -1,22 +1,16 @@
 import React, { useMemo } from "react";
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  MarkerF,
+  useJsApiLoader,
+} from "@react-google-maps/api";
 // import { business } from "./mockData";
 import "./GoogleMaps.css";
-
-// const latMarker: number = business[0].coordinates.latitude;
-// const lngMarker: number = business[0].coordinates.longitude;
-
-// console.log(latMarker, lngMarker);
 
 const containerStyle = {
   height: "100vh",
   width: "100%",
 };
-
-// const markerPosition = {
-//   lat: latMarker,
-//   lng: lngMarker,
-// };
 
 // testing data
 const locations = [
@@ -32,34 +26,37 @@ const locations = [
     lat: 37.832,
     lng: -122.424,
   },
+  {
+    lat: 42.962891037731644,
+    lng: -85.67038868154219,
+  },
 ];
 
 const TripGoogleMaps: React.FC = () => {
-  const center = useMemo(() => ({ lat: 37.09024, lng: -95.712891 }), []);
+  const center = useMemo(
+    () => ({ lat: 37.09024, lng: -95.712891 }),
+    []
+  );
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
   });
 
-  // console.log('ISLOADED: ', isLoaded);
-
-  // const mapProps = {
-  //   center,
-  //   zoom: 4.5,
-  // };
-
-  const onLoad = (marker: any) => {
+  const onLoad = (marker: object) => {
     console.log("marker: ", marker);
   };
 
   return isLoaded ? (
-    <GoogleMap mapContainerStyle={containerStyle} zoom={4.5} center={center}>
-      // TODO MARKER NOT WORKING
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      zoom={4}
+      center={center}
+    >
       {locations?.length &&
         locations?.map((markerPos) => (
-          <Marker
+          <MarkerF
             key={locations.indexOf(markerPos)}
-            onLoad={onLoad}
             position={markerPos}
+            onLoad={onLoad}
           />
         ))}
     </GoogleMap>
