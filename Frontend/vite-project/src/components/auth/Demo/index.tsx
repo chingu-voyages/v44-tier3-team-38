d47d1078fdf081301bb3c75d24c9dd69560f5e6a
Trevor Mathiak <tmathiak@gmail.com>
2023-06-21 13:21:44 -0400

    Changed password to 'snow' for demo user. Removed unused imports

--- Frontend/vite-project/src/components/auth/Demo/index.tsx
+++ Frontend/vite-project/src/components/auth/Demo/index.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../../../store/session";
 import { SyntheticEvent } from "react";
-import { ThunkDispatch } from "redux-thunk";
-import { AnyAction } from "redux";
 
 function Demo() {
   const dispatch = useDispatch();
   const demoLogin = async (event: SyntheticEvent) => {
     event.preventDefault();
     const username = "jon";
-    const password = "jon";
+    const password = "snow";
     try {
       await dispatch(login(username, password));
     } catch (error) {
