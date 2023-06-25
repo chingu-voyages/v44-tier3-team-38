fd5e9686e181bcb4c0e43eeadd91eff1c8179553
Trevor Mathiak <tmathiak@gmail.com>
2023-06-25 12:34:50 -0400

    Installed packages. Updated CreateTrip and added to Trips pages

--- Frontend/vite-project/src/components/CreateTrip.tsx
+++ Frontend/vite-project/src/components/CreateTrip.tsx
@@ -1,7 +1,35 @@
-import { FC } from "react";
+import React, { FC } from "react";
+import dayjs, { Dayjs } from "dayjs";
+import { Container, Button, Input, FormLabel } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 const CreateTrip: FC = () => {
-  return <div>CreateTrip</div>;
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs());
+
+  const handleSubmit = (
+    event: React.ChangeEvent<HTMLFormElement>
+  ) => {};
+
+  return (
+    <div>
+      <h3>Create a Trip</h3>
+      <Container maxWidth="md">
+        <form onSubmit={handleSubmit}>
+          <FormLabel>Trip Title: </FormLabel>
+          <Input placeholder="My Trip" />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Trip Date"
+              value={value}
+              onChange={(newValue) => setValue(newValue)}
+            />
+          </LocalizationProvider>
+        </form>
+      </Container>
+    </div>
+  );
 };
 
 export default CreateTrip;
--- Frontend/vite-project/src/components/TripsPage/index.tsx
+++ Frontend/vite-project/src/components/TripsPage/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TripGoogleMaps from "../GoogleMaps/index";
 import "./Trips.css";
+import CreateTrip from "../CreateTrip";
 
 const Trips = () => {
   return (
     <div className="main-trip-div-page">
       <h3>Trips</h3>
+      <CreateTrip />
       <div className="trip-google-maps">
         <TripGoogleMaps />
       </div>
